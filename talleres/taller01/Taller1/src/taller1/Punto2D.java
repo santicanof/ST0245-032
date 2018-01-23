7e34b83643f3a5cf9488077c4497c8495f7ed5d1
ARico10 <35715017+ARico10@users.noreply.github.com>
2018-01-23 11:50:11 -0500

    Add files via upload

---
+++ talleres/taller01/Taller1/src/taller1/Punto2D.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taller1;
+
+/**
+ *
+ * @author aricop
+ */
+public class Punto2D {
+    private float x;
+    private float y;
+
+    public Punto2D(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public float distancia(Punto2D otroPunto) {
+        return (float) Math.sqrt(Math.pow(x-otroPunto.getX(),2) +
+                Math.pow(y-otroPunto.getY(),2));
+                
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+    
+    public float radio() {
+        return (float) Math.sqrt((x*x)+(y*y));
+    }
+    
+    public float angulo() {
+        return (float)  Math.atan2(x,y);    
+    }
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
